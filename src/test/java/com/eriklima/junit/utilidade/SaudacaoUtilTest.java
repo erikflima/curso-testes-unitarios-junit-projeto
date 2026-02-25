6fb0c1e629323ebbec008ea4547c8ad3c96a37e2
Erik Lima <erik.f.alves.lima@gmail.com>
2026-02-25 11:00:10 -0300

    2.2. Escrevendo o seu primeiro teste unitário

---
+++ src/test/java/com/eriklima/junit/utilidade/SaudacaoUtilTest.java
@@ -0,0 +1,18 @@
+package com.eriklima.junit.utilidade;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SaudacaoUtilTest {
+
+    @Test
+    public void saudar(){
+
+        String saudacao = SaudacaoUtil.saudar(9);
+        System.out.println( saudacao );
+
+        Assertions.assertTrue( saudacao.equals("Bom dia") );
+
+    }
+
+}
