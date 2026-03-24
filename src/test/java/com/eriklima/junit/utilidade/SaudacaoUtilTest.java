28a5e2353d4c2369ec997ceffda14e9f7448bcfd
Erik Lima <erik.f.alves.lima@gmail.com>
2026-03-24 02:29:05 -0300

    2.4. Asserções de Exceptions

--- src/test/java/com/eriklima/junit/utilidade/SaudacaoUtilTest.java
+++ src/test/java/com/eriklima/junit/utilidade/SaudacaoUtilTest.java
@@ -12,7 +12,21 @@ class SaudacaoUtilTest {
         System.out.println( saudacao );
 
         assertEquals("Bom dia", saudacao,"Saudação incorreta!");
-
     }
 
+    @Test
+    public void deveLancarException(){
+
+        //Verifica se ao chamar o metodo "SaudacaoUtil.saudar(-10)" vai ocorrer uma exception do tipo "IllegalArgumentException"
+        IllegalArgumentException illegalArgumentException = assertThrows( IllegalArgumentException.class, ()-> SaudacaoUtil.saudar(-10) );
+
+        assertEquals("Hora inválida", illegalArgumentException.getMessage() );
+    }
+
+    @Test
+    public void naoDeveLancarException(){
+
+        //Verifica se ao chamar o metodo  "SaudacaoUtil.saudar(0)" nenhuma exception vai ocorrer.
+        assertDoesNotThrow( () -> SaudacaoUtil.saudar(0) );
+    }
 }
