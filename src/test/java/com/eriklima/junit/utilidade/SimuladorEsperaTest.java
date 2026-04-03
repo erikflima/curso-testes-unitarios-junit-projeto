0d0097f2ee87916fd7862c337944fbdad196df72
Erik Lima <erik.f.alves.lima@gmail.com>
2026-04-03 20:15:10 -0300

    2.9. Desabilitando testes unitários

--- src/test/java/com/eriklima/junit/utilidade/SimuladorEsperaTest.java
+++ src/test/java/com/eriklima/junit/utilidade/SimuladorEsperaTest.java
@@ -1,5 +1,6 @@
 package com.eriklima.junit.utilidade;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import java.time.Duration;
 
@@ -7,6 +8,17 @@ class SimuladorEsperaTest {
 
     @Test
     public void DeveEsperarENaoDarTimeout() {
+
+        //Verifico se o metodo que chamei vai ser executado em até o valor que passo como primeiro parametro.
+        Assertions.assertTimeout( Duration.ofSeconds(10),() -> SimuladorEspera.esperar( Duration.ofSeconds(5) ) );
+
+        Assertions.assertTimeoutPreemptively( Duration.ofSeconds(6), ()-> SimuladorEspera.esperar( Duration.ofSeconds(5) ) );
+
+    }
+
+    @Test
+    @Disabled("Esse teste não é mais aplicavel!")
+    public void DeveEsperarENaoDarTimeout2() {
 
         //Verifico se o metodo que chamei vai ser executado em até o valor que passo como primeiro parametro.
         Assertions.assertTimeout( Duration.ofSeconds(10),() -> SimuladorEspera.esperar( Duration.ofSeconds(5) ) );
