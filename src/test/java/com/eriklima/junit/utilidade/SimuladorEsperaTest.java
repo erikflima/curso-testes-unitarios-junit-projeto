8c9d14f5c680641244278fffed8110315d98deb6
Erik Lima <erik.f.alves.lima@gmail.com>
2026-04-03 21:22:43 -0300

    2.10. Ignorando execução dos testes condicionalmente com Assumptions

--- src/test/java/com/eriklima/junit/utilidade/SimuladorEsperaTest.java
+++ src/test/java/com/eriklima/junit/utilidade/SimuladorEsperaTest.java
@@ -1,7 +1,10 @@
 package com.eriklima.junit.utilidade;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+
 import java.time.Duration;
 
 class SimuladorEsperaTest {
@@ -19,6 +22,31 @@ class SimuladorEsperaTest {
     @Test
     @Disabled("Esse teste não é mais aplicavel!")
     public void DeveEsperarENaoDarTimeout2() {
+
+        //Verifico se o metodo que chamei vai ser executado em até o valor que passo como primeiro parametro.
+        Assertions.assertTimeout( Duration.ofSeconds(10),() -> SimuladorEspera.esperar( Duration.ofSeconds(5) ) );
+
+        Assertions.assertTimeoutPreemptively( Duration.ofSeconds(6), ()-> SimuladorEspera.esperar( Duration.ofSeconds(5) ) );
+
+    }
+
+    @Test
+    public void DeveEsperarENaoDarTimeout3() {
+
+        //Verifica se existe a variavel de ambiente "ENV" e se ela tem o valor 'PROD'. Se a variavel nao existir e/ou se o valor não for o esperado, então o JUnit ignora o teste/
+        Assumptions.assumeTrue( "PROD".equals(System.getenv( "ENV") ), () -> "Abortando teste: Não deve ser executado em PROD");
+
+        //Verifico se o metodo que chamei vai ser executado em até o valor que passo como primeiro parametro.
+        Assertions.assertTimeout( Duration.ofSeconds(10),() -> SimuladorEspera.esperar( Duration.ofSeconds(5) ) );
+
+        Assertions.assertTimeoutPreemptively( Duration.ofSeconds(6), ()-> SimuladorEspera.esperar( Duration.ofSeconds(5) ) );
+
+    }
+
+    //Anotacao que verifica se existe a variavel de ambiente "ENV" e se ela tem o valor 'DEV'. Se a variavel nao existir e/ou se o valor não for o esperado, então o JUnit ignora o teste/.
+    @EnabledIfEnvironmentVariable(named = "ENV", matches = "DEV")
+    @Test
+    public void DeveEsperarENaoDarTimeout4() {
 
         //Verifico se o metodo que chamei vai ser executado em até o valor que passo como primeiro parametro.
         Assertions.assertTimeout( Duration.ofSeconds(10),() -> SimuladorEspera.esperar( Duration.ofSeconds(5) ) );
